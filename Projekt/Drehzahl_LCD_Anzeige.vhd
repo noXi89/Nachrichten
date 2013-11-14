1cfb19a94b7e5b78bc040b8fe858a0b06ba00192
Mizar999 <mizar2502@aol.com>
2013-11-14 17:15:05 +0100

    Anpassungen an VHD und Testblock

--- Projekt/Drehzahl_LCD_Anzeige.vhd
+++ Projekt/Drehzahl_LCD_Anzeige.vhd
@@ -9,12 +9,13 @@ entity Drehzahl_LCD_Anzeige is
 		clock			: in std_logic := '0';
 		reset			: in std_logic := '1';
 		error			: in std_logic := '1';
-		charposition: in integer range 0 to 159;
+		-- charposition: in integer range 0 to 159;
+		charposition: in std_logic_vector(7 downto 0) := "00000000";
 		drehzahl		: in std_logic_vector(15 downto 0) := "0000000000000000";
 		richtung		: in std_logic := '0';
 		
-		ascii_out	: out std_logic_vector(7 downto 0);
-		write_enable: out std_logic
+		ascii_out	: out std_logic_vector(7 downto 0) := x"20";
+		write_enable: out std_logic := '0'
 	);
 end entity;
 
@@ -24,6 +25,7 @@ architecture rtl of Drehzahl_LCD_Anzeige is
 		process(clock, reset, error, charposition, drehzahl, richtung)
 			variable counter: integer := 0;
 			variable int_val: integer := 0;
+			variable int_charposition: integer := 0;
 			
 			variable value_position_1: integer := 0;
 			variable value_position_10: integer := 0;
@@ -32,29 +34,45 @@ architecture rtl of Drehzahl_LCD_Anzeige is
 			variable value_position_10000: integer := 0;
 			
 		begin
-			ascii_out <= x"20"; -- Leerzeichen
-			if(charposition >= 0 AND charposition <= 159) then
-				write_enable <= '1';
-			else
-				write_enable <= '0';
-			end if;
-			
 			if(reset = '0') then
+				int_charposition := to_integer(ieee.numeric_std.unsigned(charposition));
 				current_drehzahl <= "0000000000000000";
+				
+				if(int_charposition >= 0 AND int_charposition <= 159) then
+				-- if(charposition >= 0 AND charposition <= 159) then
+					write_enable <= '1';
+				else
+					write_enable <= '0';
+				end if;
+				
 				-- Gibt nur noch das Wort "RESET" aus
-				case charposition is
+				case int_charposition is
+				-- case charposition is
 					when 1 => ascii_out <= x"52"; -- R
 					when 2 => ascii_out <= x"45"; -- E
 					when 3 => ascii_out <= x"53"; -- S
 					when 4 => ascii_out <= x"45"; -- E
 					when 5 => ascii_out <= x"54"; -- T
+					when others => ascii_out <= x"20"; -- Leerzeichen;
 				end case;
 			elsif(rising_edge(clock)) then
+				int_charposition := to_integer(ieee.numeric_std.unsigned(charposition));
+				ascii_out <= x"20"; -- Leerzeichen
+				
+				if(int_charposition >= 0 AND int_charposition <= 159) then
+				-- if(charposition >= 0 AND charposition <= 159) then
+					write_enable <= '1';
+				else
+					write_enable <= '0';
+				end if;
+				
 				if(error = '1') then
 					-- Bei Fehler -> Ersetze Drehzahl- und Richtungsausgabe durch Sternchen
-					if(charposition >= 1 AND charposition <= 5) then
+					if(int_charposition >= 1 AND int_charposition <= 5) then
+					-- if(charposition >= 1 AND charposition <= 5) then
 						ascii_out <= x"2A"; -- *
-					elsif(charposition >= 40 AND charposition <= 45) then
+					elsif(int_charposition >= 40 AND int_charposition <= 45) then
+					-- elsif(charposition >= 40 AND charposition <= 45) then
 						ascii_out <= x"2A"; -- *
 					end if;
 				else
@@ -67,6 +85,7 @@ architecture rtl of Drehzahl_LCD_Anzeige is
 						while int_val >= 10000 loop
 							int_val := int_val - 10000;
 							counter := counter + 1;
+							exit when counter >= 9;
 						end loop;
 						value_position_10000 := counter;
 						
@@ -74,6 +93,7 @@ architecture rtl of Drehzahl_LCD_Anzeige is
 						while int_val >= 1000 loop
 							int_val := int_val - 1000;
 							counter := counter + 1;
+							exit when counter >= 9;
 						end loop;
 						value_position_1000 := counter;
 						
@@ -81,6 +101,7 @@ architecture rtl of Drehzahl_LCD_Anzeige is
 						while int_val >= 100 loop
 							int_val := int_val - 100;
 							counter := counter + 1;
+							exit when counter >= 9;
 						end loop;
 						value_position_100 := counter;
 						
@@ -88,6 +109,7 @@ architecture rtl of Drehzahl_LCD_Anzeige is
 						while int_val >= 10 loop
 							int_val := int_val - 10;
 							counter := counter + 1;
+							exit when counter >= 9;
 						end loop;
 						value_position_10 := counter;
 						
@@ -95,12 +117,14 @@ architecture rtl of Drehzahl_LCD_Anzeige is
 						while int_val > 1 loop
 							int_val := int_val - 1;
 							counter := counter + 1;
+							exit when counter >= 9;
 						end loop;
 						value_position_1 := counter;
 					end if;
 					
 					-- Ausgabe der einzelnen ermittelten Ziffern
-					case charposition is
+					case int_charposition is
+					-- case charposition is
 						when 1 =>
 							if(value_position_10000 = 0) then
 								ascii_out <= x"20";
@@ -127,36 +151,44 @@ architecture rtl of Drehzahl_LCD_Anzeige is
 							end if;
 						when 5 =>
 							ascii_out <= conv_std_logic_vector(48 + value_position_1, 8); -- ASCII-Null + Ziffer
+						when others =>
+							null;
 					end case;
 					
 					-- Ausgabe der Richtung
 					if(richtung = '0') then
-						case charposition is
+						case int_charposition is
+						-- case charposition is
 							when 41 => ascii_out <= x"6C"; -- l
 							when 42 => ascii_out <= x"69"; -- i
 							when 43 => ascii_out <= x"6E"; -- n
 							when 44 => ascii_out <= x"6B"; -- k
 							when 45 => ascii_out <= x"73"; -- s
+							when others => null;
 						end case;
 					else
-						case charposition is
+						case int_charposition is
+						-- case charposition is
 							when 40 => ascii_out <= x"72"; -- r
 							when 41 => ascii_out <= x"65"; -- e
 							when 42 => ascii_out <= x"63"; -- c
 							when 43 => ascii_out <= x"68"; -- h
 							when 44 => ascii_out <= x"74"; -- t
 							when 45 => ascii_out <= x"73"; -- s
+							when others => null;
 						end case;
 					end if;
 				end if; -- end if error
 				
 				-- Ausgabe der Masseinheit
-				case charposition is
+				case int_charposition is
+				-- case charposition is
 					when 7  => ascii_out <= x"55"; -- U
 					when 8  => ascii_out <= x"2F"; -- /
 					when 9  => ascii_out <= x"4D"; -- M
 					when 10 => ascii_out <= x"69"; -- i
 					when 11 => ascii_out <= x"6E"; -- n
+					when others => null;
 				end case;
 			end if;
 		end process;
